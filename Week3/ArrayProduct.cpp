d65f2346b8238f46e750ca7be7a17e6148e7dd3c
shubhamagarwal <shubhamagarwal0501@gmail.com>
2020-04-16 15:30:47 +0530

    Solutions for week3

---
+++ Week3/ArrayProduct.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n);
+        left[0]=1;
+        vector<int> right(n);
+  
+        right[0] =1;
+        for(int i=1;i<n;i++){
+            left[i] = nums[i-1] * left[i-1]; 
+            right[i] = nums[n-i] * right[i-1];
+        }
+
+        vector<int> num(n);
+        for(int i=0;i<n;i++){
+            num[i] = left[i]*right[n-1-i];
+        }
+        return num;
+    }
+
+};
+
+
